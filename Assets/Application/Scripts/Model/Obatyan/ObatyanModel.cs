cd07da8b98897c8327d14ddaddeee4631392ece1
raoka0000 <raoka0000@gmail.com>
2019-02-20 19:28:25 +0900

    おばちゃん図鑑のパッケージをインストール

---
+++ Assets/Application/Scripts/Model/Obatyan/ObatyanModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObatyanModel{
+    public string name;
+    public string place;
+    public string age;
+    public string[] description = new string[4];
+    public int getCount;
+}
---
+++ Assets/Application/Scripts/ViewModel/ObatyanListViewModel/ButtonScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ButtonScript : MonoBehaviour
+{
+    public GameObject GoalText;
+    public void ButtonPush()
+    {
+        Debug.Log("Button Push !!");
+    }
+
+    public Sprite _on;
+    public Sprite _off;
+    private bool flg = true;
+
+    public void changeImage()
+    {
+        var img = GetComponent<Image>();
+        img.sprite = (flg) ? _on : _off;
+        flg = !flg;
+    }
+
+    public void Touch()
+    {
+        gameObject.SetActive(true);
+    }
+
+
+}
