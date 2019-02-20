4141453d1407afd6ca7abf9c6a5755e1ac13136b
raoka0000 <raoka0000@gmail.com>
2019-02-20 21:09:46 +0900

    AR対応

--- Assets/Application/Scripts/View/Attribute/FlickAttribute.cs
+++ Assets/Application/Scripts/View/Attribute/FlickAttribute.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class FlickAttrubute : MonoBehaviour{
-    // Start is called before the first frame update
-    void Start()
+public class FlickAttrubute : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler{
+
+    private Vector3 acceleration;
+    private Vector3 preAcceleration;
+
+
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void OnDrag(PointerEventData eventData)
     {
-        
+        var delta = new Vector3(eventData.delta.x, eventData.delta.y, 0f);
+        preAcceleration = acceleration;
+        acceleration = delta;
+
     }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+    }
+
 }
